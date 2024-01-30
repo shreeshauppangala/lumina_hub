6d08727db9a976ce7adab2fb8b2b1f3706084d10
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-01-30 15:09:57 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,40 @@
 'use client'
 
-import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, styled } from "@mui/material";
 
-// export const CustomAppBar = styled(AppBar)(({ theme }) => ({
-//   backgroundColor: theme.palette.common.white,
-//   boxShadow: 'none',
-//   borderBottom: `${theme.spacing(0.5)} solid ${theme.palette.grey[200]}`,
-//   padding: theme.spacing(5),
-// }));
+const CustomAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.common.white,
+  boxShadow: 'none',
+  borderBottom: `${theme.spacing(0.5)} solid ${theme.palette.grey[200]}`,
+  padding: theme.spacing(5),
+}));
 
-// export const CustomToolbar = styled(Toolbar)(() => ({
-//   display: 'flex',
-//   justifyContent: 'end'
-// }));
+const CustomToolbar = styled(Toolbar)(() => ({
+  display: 'flex',
+  justifyContent: 'end'
+}));
 
-// export const SignInButton = styled(Button)(({ theme }) => ({
-//   backgroundColor: theme.palette.common.white,
-//   color: theme.palette.grey[700],
-//   boxShadow: `${theme.spacing(0, 1, 2)} rgba(3,3,3,0.1)`,
-//   '&:hover': {
-//     backgroundColor: theme.palette.common.white, color: theme.palette.grey[700],
-//     boxShadow: `${theme.spacing(0, 1, 2)} rgba(3,3,3,0.1)`,
-//     border: 'none',
-//   },
-// }))
+const SignInButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.common.white,
+  color: theme.palette.grey[700],
+  boxShadow: `${theme.spacing(0, 1, 2)} rgba(3,3,3,0.1)`,
+  '&:hover': {
+    backgroundColor: theme.palette.common.white, color: theme.palette.grey[700],
+    boxShadow: `${theme.spacing(0, 1, 2)} rgba(3,3,3,0.1)`,
+    border: 'none',
+  },
+}))
 
 const Home = () => (
   <Box>
-    <AppBar position='fixed'>
-      <Toolbar>
+    <CustomAppBar position='fixed'>
+      <CustomToolbar>
         <Box display='flex' gap={12}>
           <Button color="primary" variant='contained'>Sign Up</Button>
-          {/* <SignInButton variant="contained">Sign In</SignInButton> */}
+          <SignInButton variant="contained">Sign In</SignInButton>
         </Box>
-      </Toolbar>
-    </AppBar>
+      </CustomToolbar>
+    </CustomAppBar>
   </Box>
 );
 
