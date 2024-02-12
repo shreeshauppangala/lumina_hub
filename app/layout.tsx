06c656bb0665f7c0678e81612fca374b7bcf1aa0
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-02-12 10:21:28 +0530

    import fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import { Roboto } from 'next/font/google';
 import { Box } from '@mui/material';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import * as muiCustomTheme from '@/app/theme/theme';
-import './globals.scss';
 import Footer from './Components/Layout/Footer';
 import Header from './Components/Layout/Header';
 import ContextContainer from './context';
+import './globals.scss';
 
 const roboto = Roboto({
   subsets: ['latin'],
