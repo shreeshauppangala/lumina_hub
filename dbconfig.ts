ca1c50cabb2e90022564ef3ce87d6235cf32b26e
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-02-13 11:42:20 +0530

    db config styling console

--- dbconfig.ts
+++ dbconfig.ts
@@ -5,13 +5,19 @@ export const connect = async () => {
     mongoose.connect(process.env.MONGO_URL!);
     const { connection } = mongoose;
     connection.on('connected', () => {
-      console.log('Database connected');
+      console.log('⚡\x1b[92m Database Connected Successfully!\x1b[92m\x1b[0m⚡');
     });
     connection.on('error', (err) => {
-      console.error(err);
+      console.error(
+        '\x1b[91m Database Connection failed:\x1b[91m',
+        `\x1b[91m ${err} \x1b[91m \x1b[0m`,
+      );
       process.exit();
     });
   } catch (error) {
-    console.log(error);
+    console.error(
+      '\x1b[91m Could Not Connect to Database :\x1b[91m',
+      `\x1b[91m ${err} \x1b[91m \x1b[0m`,
+    );
   }
 };
