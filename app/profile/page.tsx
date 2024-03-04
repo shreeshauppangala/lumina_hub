50642c702587be93d139725a1ef63effff491016
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-03-04 15:45:10 +0530

    added breadcrumb to profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Box, Typography, styled } from '@mui/material';
 import { hooks } from '../hooks';
 import ProfileForm from './profileForm';
+import { Breadcrumb } from '../Components';
 
 const ProfileContainer = styled(Box)(({ theme }) => ({
   padding: theme.spacing(15, 10),
@@ -22,6 +23,7 @@ const Profile = () => {
 
   return (
     <ProfileContainer>
+      <Breadcrumb item={[{ name: 'Home', link: '/' }, { name: 'Profile' }]} />
       {isLoading ? <Typography>loading</Typography> : <ProfileForm formData={formData!} />}
     </ProfileContainer>
   );
