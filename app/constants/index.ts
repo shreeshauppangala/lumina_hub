343e4358ce037940beb2c40753e247cbfd630a3f
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-03-04 18:47:28 +0530

    cmt fix

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -16,7 +16,6 @@ const pattern = {
   aadhar: /^\d{4}\d{4}\d{4}$/g,
   panNo: /[A-Z]{5}\d{4}[A-Z]/,
   gstNo: /^\d{2}[A-Z]{5}\d{4}[A-Z][1-9A-Z]Z[0-9A-Z]$/,
-  otp: /^(\d{6})$/,
   pinCode: /^[1-9]\d{5}$/,
   bank_account: /^\d{9,18}$/,
   ifsc: /^[A-Za-z]{4}[a-zA-Z0-9]{7}$/,
@@ -25,11 +24,13 @@ const pattern = {
   rupees: /^\d*(?:\.\d*)?$/,
   removeSpecialChars: /^[A-Za-z0-9 ]+$/,
   allowOnlyNumbers: /^(\s*|\d+)$/,
-  // Requires at least one lowercase letter.
-  // Requires at least one uppercase letter.
-  // Requires at least one digit.
-  // Requires at least one special character
-  // Matches a minimum of 8 characters from the specified character classes.
+  /**
+   * @requires lowercase letter.
+   * @requires uppercase letter.
+   * @requires number.
+   * @requires special character.
+   * @satisfies a minimum of 8 characters from the specified character classes.
+   */
   strongPassword:
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
 };
