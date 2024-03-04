91f3d85f63912b70fdcb4c339742dd31634acf23
Shreesha Sharma U <shreesha.s@pacewisdom.com>
2024-03-04 16:19:58 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  target: 'experimental-serverless-trace',
-  basePath: '/app',
-  // Add the export configuration
-  exportPathMap: () => ({
-    '/': { page: '/' },
-    // Add other routes as needed
-  }),
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
